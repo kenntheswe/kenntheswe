bcdd557247cc5943575e0fe0895ada15b3218485
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-24 03:56:49 +0900

    Added about

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Hero extends Component {
+  render() {
+    return (
+      <div>
+        {/* <h1>Kenn Vun Gan</h1>
+        <h2>Full Stack Developer</h2> */}
+      </div>
+    )
+  }
+}
