48bd61473cad807efd91d381f28110bde8a704c7
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-24 13:14:46 +0900

    Added hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Hero extends Component {
   render() {
     return (
-      <div>
-        {/* <h1>Kenn Vun Gan</h1>
-        <h2>Full Stack Developer</h2> */}
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="font-serif italic lg:text-7xl md:text-5xl sm:text-3xl text-xl font-bold">Kenn Vun Gan</h1>
+        <h1 className="lg:text-7xl md:text-5xl sm:text-3xl text-xl font-light">Full Stack Developer</h1>
       </div>
     )
   }
