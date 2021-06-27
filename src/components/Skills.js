3521df19c3ce8529233118b075a8a4ce8441696f
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-27 23:39:48 +0900

    Added projects

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,7 @@ export default class Skills extends Component {
   render() {
     return (
       <div className="h-screen flex flex-col">
-        <div className="flex justify-end">
+        <div className="flex justify-end sticky top-0">
           <h1 id="skills">Skills</h1>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
