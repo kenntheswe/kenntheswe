9b190f8b9582c8bc0e600a1ac6f5e8a906f0bb19
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-28 19:02:58 +0900

    Added footer

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ import profile from '../images/profile.jpg';
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <div className="flex sticky top-0 m-4">
+      <div className="h-screen">
+        <div className="flex sticky top-0 ml-6 mb-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
           </svg>
           <h1 id="about">About</h1>
         </div>
         <div className="flex">
-          <img src={profile} alt="profile" className="rounded-lg w-48 m-4"  />
-          <p className="text-left mt-4 mx-4">Hi, I am a <strong>Full Stack Web Developer</strong> based in Tokyo, Japan. After working as non-tech roles for almost 5 years, I decided to take the leap to pursue a career in IT & software development. I enrolled in a full-time immersive web development course at Le Wagon in Tokyo. Having graduated from this intensive program, I affirm that this is the best life-changing decision I've ever made in my life. I love learning new technologies practically by building projects.</p>
+          <img src={profile} alt="profile" className="rounded-lg w-48 mx-6"  />
+          <p className="text-left mt-4 mr-6">Hi, I am a <strong>Full Stack Web Developer</strong> based in Tokyo, Japan. After working as non-tech roles for almost 5 years, I decided to take the leap to pursue a career in IT & software development. I enrolled in a full-time immersive web development course at Le Wagon in Tokyo. Having graduated from this intensive program, I affirm that this is the best life-changing decision I've ever made in my life. I love learning new technologies practically by building projects.</p>
         </div>
       </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,11 +22,11 @@ export default class Navbar extends Component {
               d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </div>
-        <div className="p-6 md:block hidden">
+        <div className="p-4 md:block hidden">
           <Link className="p-4" to="about" spy={true} smooth={true}>About</Link>
           <Link className="p-4" to="skills" spy={true} smooth={true}>Skills</Link>
           <Link className="p-4" to="projects" spy={true} smooth={true}>Projects</Link>
-          <Link className="p-4" to="contact" spy={true} smooth={true}>HIRE ME!</Link>
+          <Link to="contact" spy={true} smooth={true}><button className="focus:outline-none bg-green-400 hover:bg-green-300 px-4 py-2 rounded transition-colors duration-300 ease-in-out">HIRE ME!</button></Link>
         </div>
       </nav>
     )
