2033faaf5051df4aec3830669d43443b49a08f1c
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-09-25 22:20:11 +0900

    Fixed footer right margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center mr-4">
       <div className="flex space-x-3 ml-4 mb-4 text-3xl">
         <a href="https://www.linkedin.com/in/kennvungan/" target="_blank" rel="noreferrer nofollow"><i className="devicon-linkedin-plain colored hover:opacity-70 transition-opacity duration-300"></i></a>
         <a href="https://github.com/Kenn-0" target="_blank" rel="noreferrer nofollow"><i style={{ color:"grey" }} className="devicon-github-original colored hover:opacity-70 transition-opacity duration-300"></i></a>
