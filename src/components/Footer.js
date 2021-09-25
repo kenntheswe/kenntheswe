2d81876eb787611bd79f73d40d8cd8a7f3c262b1
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-09-25 22:07:29 +0900

    Fixed footer margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-
   return (
-    <div className="flex justify-between items-center my-3 mx-6">
-      <div className="flex space-x-3 text-3xl">
-        <a href="https://www.linkedin.com/in/kennvungan/" target="_blank" rel="noreferrer nofollow"><i className="devicon-linkedin-plain colored hover:opacity-80 transition-opacity duration-300"></i></a>
-        <a href="https://github.com/Kenn-0" target="_blank" rel="noreferrer nofollow"><i className="devicon-github-original colored hover:opacity-80 transition-opacity duration-300"></i></a>
+    <div className="flex justify-between items-center">
+      <div className="flex space-x-3 ml-4 mb-4 text-3xl">
+        <a href="https://www.linkedin.com/in/kennvungan/" target="_blank" rel="noreferrer nofollow"><i className="devicon-linkedin-plain colored hover:opacity-70 transition-opacity duration-300"></i></a>
+        <a href="https://github.com/Kenn-0" target="_blank" rel="noreferrer nofollow"><i className="devicon-github-original colored hover:opacity-70 transition-opacity duration-300"></i></a>
       </div>
       <p>&copy; 2021 Kenn's. All Rights Reserved.</p>
     </div>
