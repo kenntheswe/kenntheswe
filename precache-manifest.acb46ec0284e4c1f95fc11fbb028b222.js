fd527b4a7d61800544d2539a4430a544ca02b45c
Kenn Vun Gan <kennvun@hotmail.com>
2022-10-17 00:45:54 +0900

    Updates

---
+++ precache-manifest.acb46ec0284e4c1f95fc11fbb028b222.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "372842a69fd1f08834cf",
+    "url": "/portfolio/static/css/main.6798dabc.chunk.css"
+  },
+  {
+    "revision": "372842a69fd1f08834cf",
+    "url": "/portfolio/static/js/main.d76f594a.chunk.js"
+  },
+  {
+    "revision": "cfcc70bdf9aea1db286f",
+    "url": "/portfolio/static/js/runtime~main.474b6c12.js"
+  },
+  {
+    "revision": "4237b6ff78d1770f2f47",
+    "url": "/portfolio/static/css/2.142a397d.chunk.css"
+  },
+  {
+    "revision": "4237b6ff78d1770f2f47",
+    "url": "/portfolio/static/js/2.80e838f8.chunk.js"
+  },
+  {
+    "revision": "ab48fe92e6dd811802a1",
+    "url": "/portfolio/static/js/3.1df2cf0d.chunk.js"
+  },
+  {
+    "revision": "f5ee1af0e9118ea5f79bb2ce76372f91",
+    "url": "/portfolio/static/media/profile.f5ee1af0.jpg"
+  },
+  {
+    "revision": "a6105b1fed2158a7338873f75bda75da",
+    "url": "/portfolio/static/media/logo.a6105b1f.png"
+  },
+  {
+    "revision": "18a7d0ed666198f790b9f32bc6ca75ba",
+    "url": "/portfolio/static/media/baiku.18a7d0ed.png"
+  },
+  {
+    "revision": "5327e68e5f9077629b595e6ca2c06e66",
+    "url": "/portfolio/static/media/studioFind.5327e68e.png"
+  },
+  {
+    "revision": "423d86c59dc4b2f16ea93ae77a0934db",
+    "url": "/portfolio/static/media/watchlist.423d86c5.png"
+  },
+  {
+    "revision": "63173d2f5faae45a1990a88eec01be95",
+    "url": "/portfolio/static/media/reactGiphy.63173d2f.png"
+  },
+  {
+    "revision": "2fb333965a9a35ca48a79ec2dc461afe",
+    "url": "/portfolio/index.html"
+  }
+];
